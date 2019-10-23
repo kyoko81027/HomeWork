5df1f176dba026ea817ca666f17dd354e82b2bab
ian <kyoko81027@gmail.com>
2019-10-23 19:12:43 +0800

    final

--- venv/Master.py
+++ venv/Master.py
@@ -1,11 +1,11 @@
-import pGet_txt as pGet
-TList =pGet.temperatureList
-dicList = TList.ToList()
+import pGet_txt as pGet #引用檔案PGet_txt
+TList =pGet.temperatureList #引用檔案裡的class
+dicList = TList.ToList() #撈取資料
 for name,value in dicList.items():
-    if float(value) >=28.0:
-        print("這個月\""+name+"\"太熱了! 氣溫:"+value)
-    elif float(value) <=20.0:
-        print("這個月\"" + name + "\"太冷了! 氣溫:" + value)
+    if float(value) >=28.0: #判斷超過28度顯示太熱
+        print("這個月\""+name+"\" 氣溫:"+value+" 太熱了!")
+    elif float(value) <=20.0: #判斷小於20度顯示太冷
+        print("這個月\"" + name+"\" 氣溫:" + value+ " 太冷了!")
     else:
         print("這個月\"" + name + "\" 氣溫:" + value)
 
--- venv/pGet_txt.py
+++ venv/pGet_txt.py
@@ -2,17 +2,17 @@ class temperatureList():
 
     def OpenExcel():
         f =open('氣候月平均.txt')  # txt檔案和當前指令碼在同一目錄下，所以不用寫具體路徑
-        p = f.readlines()
+        p = f.readlines()  #把每一行讀出來
         #emperatureList.ToList(p)
         return p
 
         #print(p)
     def ToList():
-        data = temperatureList.OpenExcel()
-        dicList= {}
-        for pArry in data:
-            Restr = pArry.replace('\n','')
-            str = Restr.split('\t')
+        data = temperatureList.OpenExcel() #讀txt
+        dicList= {} #宣告Dictionaries
+        for pArry in data: #跑回圈把資料塞到Dictionaries裡
+            Restr = pArry.replace('\n','') #把換行符號拿掉
+            str = Restr.split('\t') #利用空格符號切割資料
             dicList[str[0]] = str[1]
 
         #print(dicList)
