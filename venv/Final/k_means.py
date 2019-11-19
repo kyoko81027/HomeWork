3b4a1d485fe19b526e989b10ece792f0fe39f33f
ian <kyoko81027@gmail.com>
2019-11-19 19:05:31 +0800

    test

---
+++ venv/Final/k_means.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import
+
+
+
+df = pd.DataFrame({
+    'x': [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72],
+    'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
+})
+
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=5)#分三群
+kmeans.fit(df)
+y_pred = kmeans.fit_predict(df)#分成3種顏色
+
+plt.figure(figsize=(10, 6))
+plt.xlabel('')
+plt.ylabel('')
+plt.scatter(df['x'], df['y'], c=y_pred) #C是第三維度 顏色做維度
+
+centroids = kmeans.cluster_centers_ #各群中心點(X,Y)的位置
+plt.scatter(centroids[:, 0], centroids[:, 1],c='red') #標群心
+
+plt.show()
+
+kmeans.cluster_centers_ #各群中心點(X,Y)的位置
