ac2d33f78c588af39e27f51bf3c841527701935a
ian <kyoko81027@gmail.com>
2019-11-19 19:24:38 +0800

    test

--- venv/Final/k_means.py
+++ venv/Final/k_means.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import csvtest as csvt
 
+csvtemp = csvt.loadCsv
 
 
 df = pd.DataFrame({
@@ -9,16 +11,20 @@ df = pd.DataFrame({
     'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
 })
 
+df = pd.DataFrame(csvtemp.loadCSVfile2()[0])
+
+
+
 from sklearn.cluster import KMeans
 
-kmeans = KMeans(n_clusters=5)#分三群
+kmeans = KMeans(n_clusters=3)#分三群
 kmeans.fit(df)
 y_pred = kmeans.fit_predict(df)#分成3種顏色
 
 plt.figure(figsize=(10, 6))
 plt.xlabel('')
 plt.ylabel('')
-plt.scatter(df['x'], df['y'], c=y_pred) #C是第三維度 顏色做維度
+plt.scatter(df[:, 0], df[:, 1], c=y_pred) #C是第三維度 顏色做維度
 
 centroids = kmeans.cluster_centers_ #各群中心點(X,Y)的位置
 plt.scatter(centroids[:, 0], centroids[:, 1],c='red') #標群心
--- venv/csvtest.py
+++ venv/csvtest.py
@@ -4,8 +4,8 @@ class loadCsv():
    def loadCSVfile2():
       tmp = np.loadtxt("dataset.csv", dtype=np.str, delimiter=",")
       tital = tmp[0, 0:].astype(np.chararray)  # 加载数据部分
-      data = tmp[1:,0:].astype(np.float)#加载数据部分
+      data = tmp[1:,1:].astype(np.float)#加载数据部分
       label = tmp[1:,0].astype(np.float)#加载类别标签部分
       return data, label,tital #返回array类型的数据
 
-#print(loadCSVfile2())
+#print(loadCsv.loadCSVfile2())
